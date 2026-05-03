4d911e262426af3b5c80a6caf1f52468b1d94587
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2026-05-03 22:52:42 +0300

    style: add files structure for project

---
+++ film-collection/src/app/components/film-detail/film-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-detail',
+  imports: [],
+  templateUrl: './film-detail.component.html',
+  styleUrl: './film-detail.component.scss'
+})
+export class FilmDetailComponent {
+
+}
