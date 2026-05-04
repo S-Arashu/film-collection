5779618cf86e458fed395a523be2ce3fca0a649e
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2026-05-04 23:39:02 +0300

    feat: add service for app

--- film-collection/src/app/services/films.service.ts
+++ film-collection/src/app/services/films.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
+import { Film } from '../models/film.model';
+import { FILMS_DATA } from '../data/films.data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilmsService {
+  private _films = signal<Film[]>(FILMS_DATA);
 
-  constructor() { }
+  films = this._films.asReadonly();
+
+  favorites = computed(() => this._films().filter((f) => f.isFavorite));
+
+  getById(id: number): Film | undefined {
+    return this._films().find((f) => f.id === id);
+  }
+
+  toggleFavorite(id: number): void {
+    this._films.update((films) =>
+      films.map((f) => (f.id === id ? { ...f, isFavorite: !f.isFavorite } : f)),
+    );
+  }
 }
